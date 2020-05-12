58c9639805d885ee2116499130ebee51a4cb3427
pamuditha-i <49978028+pamuditha-i@users.noreply.github.com>
2020-05-12 12:49:27 +0530

    Update placing_parity_bits.c

--- placing_parity_bits.c
+++ placing_parity_bits.c
@@ -10,34 +10,30 @@ char* addParity(int bitsq_length, char* bitsq){
     {
         if(i==((int)pow(2,parityPos)-1))
         {   pariadded_array[i] = '0';  // assigning zeros to the indexes of powers of two
-            parityPos++;
-        }
+            parityPos++; }
         else
         {   pariadded_array[i]=bitsq[nonarityPos];
-            nonarityPos++;
-        }
+            nonarityPos++;   }
     }
-
+    
 // checking even parity and assigning the relevent bit to the indexes of powers of two
-
-    for(int i=0;i<no_of_parities;i++)
-    {
+    for(int i=0;i<no_of_parities;i++) {
     position = (int)pow(2,i);
     int s,count = 0;
     s = position-1;
     while(s < no_of_parities + bitsq_length)
     {
         for(int j = s; j < s+position; j++)
-        {
-            if(pariadded_array[j] == '1')
+        {     if(pariadded_array[j] == '1')
                 {count++; }
         }
-        s=s+2*position;
+        s = s + 2*position;    
     }
+        
     if(count%2 == 0)
         pariadded_array[position-1] = '0';
     else
-        pariadded_array[position-1] = '1';
+        pariadded_array[position-1] = '1'; 
     }
     return pariadded_array;
 }
